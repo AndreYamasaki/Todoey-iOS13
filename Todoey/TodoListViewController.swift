11d45d4d8700b6c29b3a642ae2d4c6ccd95503c2
Andre <andreyamasaki@brq.com>
2021-07-26 17:14:11 -0300

    added TablewVIewController and TablewViewDelegate Methods

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    //MARK: - Attributes
+    
+    let itemArray = ["test 1", "test 2", "test3"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+    }
+    
+    //MARK: - TableView DataSource
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell")
+        cell?.textLabel?.text = itemArray[indexPath.row]
+        return cell!
+    }
+    
+    //MARK: - TableView Delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
